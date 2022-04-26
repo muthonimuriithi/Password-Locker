d4bc795ff6e66d5759265e7967539beae1c325b3
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-04-26 11:04:05 +0300

    created a method to add new objcs to the credential list

---
+++ credentials.py
@@ -0,0 +1,18 @@
+class Credentials:
+     '''
+    class that creates instances of users accounts credentials
+    '''
+
+credentials_list = [] #creates an empty list for user credentials
+
+def __init__(self, account_name, first_name, last_name, user_password):
+        self.account_name = account_name
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_password = user_password
+
+def save_credential(self):
+        '''
+        Method to save a new object in the credential list
+        '''
+        Credentials.credentials_list.append(self)
