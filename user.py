08b7620b96d0d2766328bcb0670506860bb4e973
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-04-26 10:35:38 +0300

    created credentls cls

--- user.py
+++ user.py
@@ -1,17 +1,20 @@
 class User:
      '''
-    class that creates instances of users
+    class which creates instances of users
     '''
 
-# users_list = [] #creates an empty list for us
+users_list = [] #creates an empty list for users
 
 
-# def __init__(self, user_name, user_passwrd):
-#         self.user_name = user_name
-#         self.user_passwrd = user_passwrd
+def __init__(self, user_name, user_password):
+        self.user_name = user_name
+        self.user_password = user_password
 
-# def save_user(self):
-#         '''
-#         This method saves new object to users list
-#         '''
-#         User.users_list.append(self)
+def save_user(self):
+        '''
+        This method saves new object to users list
+        '''
+        User.users_list.append(self)
+
+def delete_user(self):
+            User.users_list.remove(self)
