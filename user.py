15ac5b8ac465c50f1973b833dbfe8e41bf9d4367
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-04-26 09:37:36 +0300

    initial commit:created user class

---
+++ user.py
@@ -0,0 +1,17 @@
+class User:
+     '''
+    class that creates instances of users
+    '''
+
+# users_list = [] #creates an empty list for us
+
+
+# def __init__(self, user_name, user_passwrd):
+#         self.user_name = user_name
+#         self.user_passwrd = user_passwrd
+
+# def save_user(self):
+#         '''
+#         This method saves new object to users list
+#         '''
+#         User.users_list.append(self)
