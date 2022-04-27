b11399a2779ab92db230931ba4fe2a47e2d4f10f
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-04-27 06:53:09 +0300

    add classmthd to check if user exist

--- user.py
+++ user.py
@@ -16,5 +16,34 @@ def save_user(self):
         '''
         User.users_list.append(self)
 
+        #from here
+
+@classmethod
+def find_by_user_name(cls,user_name):
+        '''
+        Method that takes in a user_name and returns a user that matches that user.
+        Args:
+            user_name: user_name to search for
+        Returns :
+            user of person that matches the number.
+        '''
+
+        for user in cls.user_list:
+            if user.user_name == user_name:
+                return user
+
+@classmethod
+def user_exist(cls,user_name):
+        '''
+        Method that checks if a user exists from the user list.
+        Args:
+            user_name: user_name to search for
+            Boolean: True or false depending if the user exists
+        '''
+        for user in cls.user_list:
+            if user.user_name == user_name:
+                    return True
+
+
 def delete_user(self):
-            User.users_list.remove(self)
+ User.users_list.remove(self)
